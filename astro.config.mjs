d1d7715b89892923f07fb9452e82f9a9ac05add6
Takaya Saito <mkylzpu123@gmail.com>
2026-07-27 16:46:14 +0900

    feat: GitHub Pagesデプロイメントワークフローを追加し、robots.txtを設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://mkylzpu1.github.io',
   integrations: [react(), tailwind()],
   i18n: {
     defaultLocale: 'ja',
